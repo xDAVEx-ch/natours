f5a8d037a87d167e5c7eea355132e1bed6fd401f
Dave Ch <dabbidchavarria@gmail.com>
2021-05-18 19:30:36 -0600

    Configure development and production enviroments with cross-env

--- utils/appError.js
+++ utils/appError.js
@@ -4,7 +4,7 @@ class AppError extends Error {
 
         this.statusCode = statusCode;
         this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-        this.isOperational = true;
+        this.isOperational = true; //Other errors (Programming errors) won't have this property
 
         //Constructor won't show up in stack trace
         Error.captureStackTrace(this, this.constructor);
